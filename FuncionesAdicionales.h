b8b9949ec008fa322ea10bf08cd9fa9fd5a5759f
Jose-Gabriel-Giraldo <josegabriel.giraldo@udea.edu.co>
2024-10-15 20:46:27 -0500

    Create FuncionesAdicionales.h

---
+++ FuncionesAdicionales.h
@@ -0,0 +1,10 @@
+#ifndef FUNCIONESADICIONALES_H
+#define FUNCIONESADICIONALES_H
+#include "RedNacional.h"
+
+void gestionDeRed(RedNacional& red);
+void gestionEstacionesDeServicio(RedNacional& red);
+void verificarFuga(RedNacional& red);
+void simulacion(RedNacional& red);
+
+#endif //FUNCIONESADICIONALES_H
